14436af0f035e2e34d68d96a6daa8696402238a6
Arman Harutyunyan <armanharut98@gmail.com>
2024-11-01 22:49:18 +0400

    ADD: sm-reader module ADD: github runner configuration

--- infra/hub/local.tf
+++ infra/hub/local.tf
@@ -3,26 +3,31 @@ module "hub_infra_parameters" {
   parameter_list = ["/hub/az_count", "/hub/vpc_cidr", "/hub/vpc_space_cidr"]
 }
 
+module "hub_secrets" {
+  source    = "../modules/sm-reader"
+  secret_id = "hub/infra-secrets"
+}
+
 data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
 
 locals {
   az_suffix = ["a", "b", "c", "d", "e", "f"]
 
   # Main Component Configurations.
-  hub_name_prefix = "${terraform.workspace}.hub"
-  region          = data.aws_region.current.name
-  hub_az_count    = module.hub_infra_parameters.map["/hub/az_count"]
-  hub_vpc_cidr    = module.hub_infra_parameters.map["/hub/vpc_cidr"]
-  vpn_cidr        = "10.20.0.0/24"
-  vpc_space_cidr  = module.hub_infra_parameters.map["/hub/vpc_space_cidr"]
-  hub_public_key  = file("${path.module}/pub_keys/${terraform.workspace}_hub_instance_access.pub")
-  //  github_token        = module.network_secret_ro.secret_map["github_token"]
-  //  repo_url            = module.network_secret_ro.secret_map["repo_url"]
-  //  runner_label        = "main-runner"
-  //  github_public_key   = file("${path.module}/pub_keys/dev_core_instance_access.pub")
-  //  ec2_runner_iam_role = module.network_secret_ro.secret_map["ec2_runner_iam_role"]
-  //  github_org          = module.network_secret_ro.secret_map["github_org"]
-  //  github_repo_name    = module.network_secret_ro.secret_map["github_repo_name"]
+  hub_name_prefix     = "${terraform.workspace}.hub"
+  region              = data.aws_region.current.name
+  hub_az_count        = module.hub_infra_parameters.map["/hub/az_count"]
+  hub_vpc_cidr        = module.hub_infra_parameters.map["/hub/vpc_cidr"]
+  vpn_cidr            = "10.20.0.0/24"
+  vpc_space_cidr      = module.hub_infra_parameters.map["/hub/vpc_space_cidr"]
+  hub_public_key      = file("${path.module}/pub_keys/${terraform.workspace}_hub_instance_access.pub")
+  github_token        = module.hub_secrets.secret_map["github_token"]
+  repo_url            = module.hub_secrets.secret_map["github_repo_url"]
+  ec2_runner_iam_role = module.hub_secrets.secret_map["ec2_runner_iam_role"]
+  github_org          = module.hub_secrets.secret_map["github_org"]
+  github_repo_name    = module.hub_secrets.secret_map["github_repo_name"]
+  runner_label        = "main-runner"
 
   # Core Component Configurations.
   core_workspaces = ["dev"]
