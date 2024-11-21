abee6cc6ccf60b855338ac610a78975bbfbb41ef
Arman Harutyunyan <110357946+armanharut98@users.noreply.github.com>
2024-11-21 19:07:43 +0400

    Minor fixes (#33)
    * minor-fix
    
    * minor-fix
    
    * minor-fix
    
    * minor fix
    
    * minor fixes
    
    * minor fix
    
    * minor fix
    
    * minor fixes

--- infra/core/ecr.tf
+++ infra/core/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = "${terraform.workspace}-app-repo"
   image_tag_mutability = "MUTABLE"
+
   image_scanning_configuration {
     scan_on_push = true
   }
